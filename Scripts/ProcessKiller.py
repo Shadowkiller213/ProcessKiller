1928ac49b51b26338100c5722b2295d56ba5f66b
Shadowkiller213 <ruslanakuev312@gmail.com>
2022-11-22 22:45:49 +0300

    Add Project

---
+++ Scripts/ProcessKiller.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import shutil
+import psutil
+import time
+
+start = time.time()
+
+while True:
+    for proc in psutil.process_iter():
+        print(proc.name())
+        if "roblox" in proc.name().lower():
+            proc.kill()
+        
+    os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\AppData\Roaming"))
+
+    for x in os.listdir(os.getcwd()):
+        if 'roblox' in x.lower() and os.path.isdir(x):
+            shutil.rmtree(x, ignore_errors=True)
+
+    os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\AppData\Local"))
+    for x in os.listdir(os.getcwd()):
+        if 'roblox' in x.lower() and os.path.isdir(x):
+            shutil.rmtree(x, ignore_errors=True)
+
+    os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\Downloads"))
+    for x in os.listdir(os.getcwd()):
+        if 'roblox' in x.lower():
+            os.remove(x)
+
+    time.sleep(0.5 * 60)
