dbb415d10c7e07d7a3e38f44730860090b0f4d71
Shadowkiller213 <ruslanakuev312@gmail.com>
2022-11-25 17:55:29 +0300

    Ignore Errors

--- Scripts/ProcessKiller.py
+++ Scripts/ProcessKiller.py
@@ -7,25 +7,28 @@ import time
 start = time.time()
 
 while True:
-    for proc in psutil.process_iter():
-        print(proc.name())
-        if "roblox" in proc.name().lower():
-            proc.kill()
+    try:
+        for proc in psutil.process_iter():
+            print(proc.name())
+            if "roblox" in proc.name().lower():
+                proc.kill()
         
-    os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\AppData\Roaming"))
+        os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\AppData\Roaming"))
 
-    for x in os.listdir(os.getcwd()):
-        if 'roblox' in x.lower() and os.path.isdir(x):
-            shutil.rmtree(x, ignore_errors=True)
+        for x in os.listdir(os.getcwd()):
+            if 'roblox' in x.lower() and os.path.isdir(x):
+                shutil.rmtree(x, ignore_errors=True)
 
-    os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\AppData\Local"))
-    for x in os.listdir(os.getcwd()):
-        if 'roblox' in x.lower() and os.path.isdir(x):
-            shutil.rmtree(x, ignore_errors=True)
+        os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\AppData\Local"))
+        for x in os.listdir(os.getcwd()):
+            if 'roblox' in x.lower() and os.path.isdir(x):
+                shutil.rmtree(x, ignore_errors=True)
 
-    os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\Downloads"))
-    for x in os.listdir(os.getcwd()):
-        if 'roblox' in x.lower():
-            os.remove(x)
+        os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\Downloads"))
+        for x in os.listdir(os.getcwd()):
+            if 'roblox' in x.lower():
+                os.remove(x)
+    except  Exception:
+        pass
 
     time.sleep(0.5 * 60)
