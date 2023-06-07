212a84bcf7da4bf89229508736e93b777fa5a547
Shadowkiller213 <56482435+Shadowkiller213@users.noreply.github.com>
2023-06-07 15:54:38 +0300

    Update ProcessKiller.py

--- Scripts/ProcessKiller.py
+++ Scripts/ProcessKiller.py
@@ -1,34 +1,42 @@
 import os
 import sys
-import shutil
-import psutil
 import time
+import shutil as shutils
+import psutil as psutils
 
 start = time.time()
+mask = ["roblox", "warthunder"]
 
 while True:
-    try:
-        for proc in psutil.process_iter():
-            print(proc.name())
-            if "roblox" in proc.name().lower():
+    for proc in psutils.process_iter():
+        if any(x in proc.name().lower() for x in mask):
+            try:
+                print(proc.name().lower())
                 proc.kill()
-        
-        os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\AppData\Roaming"))
+            except Expection as e:
+                print('Blyat', e)
 
-        for x in os.listdir(os.getcwd()):
-            if 'roblox' in x.lower() and os.path.isdir(x):
-                shutil.rmtree(x, ignore_errors=True)
+    for user in os.listdir(r'C:\Users'):
+        p = f"C:\\Users\\{user}"
+        if os.path.isdir(p):
+            r = f"{p}\\AppData\\Roaming"
+            if os.path.exists(r):
+                for x in os.listdir(r):
+                    if os.path.isdir(f"{r}\\{x}") and any(y in x.lower() for y in mask):
+                        shutils.rmtree(f"{r}\\{x}", ignore_errors=True)
 
-        os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\AppData\Local"))
-        for x in os.listdir(os.getcwd()):
-            if 'roblox' in x.lower() and os.path.isdir(x):
-                shutil.rmtree(x, ignore_errors=True)
+            r = f"{p}\\AppData\\Local"
+            if os.path.exists(r):
+                for x in os.listdir(r):
+                    if os.path.isdir(f"{r}\\{x}") and any(y in x.lower() for y in mask):
+                        shutils.rmtree(f"{r}\\{x}", ignore_errors=True)
 
-        os.chdir(os.path.expandvars(r"C:\Users\\Ученик\\Downloads"))
-        for x in os.listdir(os.getcwd()):
-            if 'roblox' in x.lower():
-                os.remove(x)
-    except  Exception:
-        pass
-
+            r = f"{p}\\Downloads"
+            if os.path.exists(r):
+                for x in os.listdir(r):
+                    if any(y in x.lower() for y in mask):
+                        os.remove(f"{r}\\{x}")
+    print('Done!')
     time.sleep(0.5 * 60)
+
+
